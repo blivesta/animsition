f0e6f353a8d0f75a983515826436fe5cead1db92
blivesta <enmt@blivesta.com>
2014-05-31 03:10:28 +0900

    update 2.2.0

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -1,63 +1,73 @@
 (function($) {
   var namespace = 'clickstream';
   var methods = {
-    init: function(options){
+    
+    init: function(options){      
       options = $.extend({
         inClass :   'clickstream-in',
         outClass :  'clickstream-out',
         linkClass : 'clickstream-link'
       }, options);
       return this.each(function(){
-        var _this = this,
-            $this = $(this),
-            data = $this.data(namespace);
+        var _this = this;
+        var $this = $(this);
+        var data = $this.data(namespace);
+        var inAnimate = $this.data('animate-in');
+        var outAnimate = $this.data('animate-out');
         if (!data) {        
           options = $.extend({}, options);
           $this.data(namespace, {
             options: options
-          });          
-
-          var inAnimate = $this.data('animate-in'),
-              outAnimate = $this.data('animate-out'),
-              inDelay =  $('.'+options.inClass).css('animation-duration').replace(/s/g,'') * 1000;
-
-          $(window).load(function() {
-            $this
-              .addClass(inAnimate);
-            setTimeout(function(){
-              $this
-                .removeClass(inAnimate +' '+ options.inClass)
-                .addClass(options.outClass)
-                .css({
-                  "opacity":1
-                });
-            },inDelay);
+          });                    
+          $(window).load(function() {   
+            methods.pageIn.call( _this , inAnimate );
           }); 
-
-          $('.'+options.linkClass).on('click.' + namespace, function(e) {
-            e.preventDefault();
-            var url = $(this).attr('href'),
-                outDelay =  $('.'+options.outClass).css('animation-duration').replace(/s/g,'') * 1000,
-                stream = function(){
-                  location.href = url;
-                };
-            $this
-              .addClass(outAnimate);
-            setTimeout(function(){
-             stream.call(_this);
-            },outDelay);        
+          $('.'+options.linkClass).on('click.' + namespace, function(event) {
+            event.preventDefault();
+            var $self = $(this);
+            methods.pageOut.call( _this,$self , outAnimate );
           });
-
         }
       }); // end each
     },
+    
+    pageIn: function(inAnimate){
+      var $this = $(this);
+      var options = $this.data(namespace).options;
+      var inDelay =  $('.'+options.inClass).css( 'animation-duration' ).replace(/s/g,'') * 1000;
+      $this.addClass(inAnimate);
+      setTimeout(function(){
+        $this
+          .removeClass(inAnimate +' '+ options.inClass)
+          .addClass(options.outClass)
+          .css({
+            "opacity":1
+          });
+      },inDelay);
+    },
+
+    pageOut: function($self,outAnimate){
+      var $this = $(this);
+      var options = $this.data(namespace).options;
+      var url = $self.attr('href');
+      var outDelay =  $('.'+options.outClass).css('animation-duration').replace(/s/g,'') * 1000;
+      var stream = function(){
+        location.href = url;
+      };
+      $this.addClass(outAnimate);
+      setTimeout(function(){
+       stream();
+      },outDelay);        
+    },
+    
     destroy: function(){
       return this.each(function(){
         var $this = $(this);
         $(window).unbind('.'+namespace);
         $this.removeData(namespace);
-      });
+      });      
     }
+    
   };
   $.fn.clickstream = function(method){
     if ( methods[method] ) {
