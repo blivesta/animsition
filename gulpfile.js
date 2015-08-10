c50293a0641751eaa4d33521d1768284df490389
blivesta <enmt@blivesta.com>
2015-08-10 19:56:45 +0900

    shift from grunt to gulp #61

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var autoprefixer = require('autoprefixer-core');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var gulp = require('gulp');
+var header = require('gulp-header');
+var jshint = require('gulp-jshint');
+var pkg = require('./package.json');
+var postcss = require('gulp-postcss');
+var cssImport = require('postcss-import');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+
+var banner = [
+'/*!',
+' * <%= pkg.name %> v<%= pkg.version %>',
+' * <%= pkg.description %>',
+' * <%= pkg.homepage %>',
+' * License : <%= pkg.license %>',
+' * Author : <%= pkg.author.name %> (<%= pkg.author.url %>)',
+' */',
+''].join('\n');
+
+var dirs = {
+  src:'./src',
+  dist:'./dist'
+};
+
+gulp.task('css', function () {
+  var processors = [
+    cssImport(),
+    autoprefixer({browsers: ['> 1%', 'last 2 versions']}),
+  ];
+  return gulp.src(dirs.src + '/css/' + pkg.name + '.css')
+    .pipe(header(banner, {pkg:pkg}))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(dirs.dist + '/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+
+gulp.task('min', function () {
+  return gulp.src(dirs.dist + '/css/' + pkg.name + '.css')
+    // .pipe(cssnano())
+    .pipe(csso())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+
+
+gulp.task('js', function(){
+  return gulp.src(dirs.src + '/js/' + pkg.name + '.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(header(banner, { pkg:pkg }))
+    .pipe(rename({prefix: 'jquery.'}))
+    .pipe(gulp.dest(dirs.dist + '/js'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(header(banner, { pkg:pkg }))
+    .pipe(gulp.dest(dirs.dist + '/js'));
+});
+
+gulp.task('cleanup', function(cb){
+  return del([ './dist' ],cb);
+});
+
+gulp.task('default',['build'], function(){
+  gulp.watch(['./src/css/**/*.css'], ['css']);
+  gulp.watch(['./src/js/*.js'], ['js']);
+});
+
+gulp.task('build', ['cleanup'], function(cb){
+  runSequence(
+
+    ['js','css',],
+    // 'min',
+    cb
+  );
+});
