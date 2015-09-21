5f79694ccafddcb004d0f8efcd6e4b8b40a4115b
blivesta <design@blivesta.com>
2015-09-21 17:40:58 +0900

    add comment for `unSupportCss`

--- src/js/animsition.js
+++ src/js/animsition.js
@@ -29,6 +29,8 @@
         unSupportCss          : [ 'animation-duration',
                                   '-webkit-animation-duration',
                                   '-o-animation-duration'],
+        // "unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
         overlay               :   false,
         overlayClass          :   'animsition-overlay-slide',
         overlayParentElement  :   'body'
